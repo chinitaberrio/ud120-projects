e8c2382b7ca3082adc4771e47b418400e4871456
Julie Stephany Berrio Perez <jber4282@sydney.edu.au>
2020-09-30 15:11:21 +1000

    feature_selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -3,15 +3,16 @@
 import pickle
 import numpy
 numpy.random.seed(42)
-
+from sklearn import tree
+from sklearn.model_selection import train_test_split
 
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
-authors_file = "../text_learning/your_email_authors.pkl"
-word_data = pickle.load( open(words_file, "r"))
-authors = pickle.load( open(authors_file, "r") )
+words_file = "word_data_overfit.pkl" 
+authors_file = "email_authors_overfit.pkl"
+word_data = pickle.load( open(words_file, "rb"))
+authors = pickle.load( open(authors_file, "rb") )
 
 
 
@@ -19,8 +20,7 @@ authors = pickle.load( open(authors_file, "r") )
 ### remainder go into training)
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
@@ -34,10 +34,18 @@ features_test  = vectorizer.transform(features_test).toarray()
 ### train on only 150 events to put ourselves in this regime
 features_train = features_train[:150].toarray()
 labels_train   = labels_train[:150]
-
-
+print (labels_train)
 
 ### your code goes here
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train,labels_train) #features and labels
+accuracy = clf.score(features_test,labels_test)
+print (accuracy)
+for i in range (2803):
+	if (clf.feature_importances_[i] != 0.0):
+		print (i, clf.feature_importances_[i])
+
+
 
 
 
